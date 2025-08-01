e198da35a4253fad5457c105679d14746e4a8ce8
mobeduck <mobedickhead@gmail.com>
2025-07-31 21:16:27 -0600

    feat: initialize project structure

---
+++ src/main/java/com/mobe/worker/TuttiFruttiZeebeWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.mobe.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuttiFruttiZeebeWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuttiFruttiZeebeWorkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobe/worker/TuttiFruttiZeebeWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobe.worker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuttiFruttiZeebeWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
